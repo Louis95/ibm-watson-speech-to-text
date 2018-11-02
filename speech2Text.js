4c1c08b249f0f526d1a1b75a69ecb28b4976392b
Asobo13 <plouisasobo@gmail.com>
2018-11-02 13:33:02 +0100

    Update speech2Text.js
    app working

--- speech2Text.js
+++ speech2Text.js
@@ -24,8 +24,7 @@ function transcribeFile(file) {
     let headers = new Headers(); //creating a new header object
     let user= "c17a629b-a1fd-476f-87cf-3869c3ff0abd"; // ibm watson speech to text username
     let password= "lEQvDlNJTBeI"; //ibm watson speech to text password
-    //let header = {'Content-Type': 'audio/flac','Transfer-Encoding': 'chunked'};
-    //let query = '?keywords=several,tornadoes,touch&max_alternatives=3&keywords_threshold=0.5';
+    
     let url = 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize';  //ibm watson speech to text url
 
     headers.set('Authorization', 'Basic ' + btoa(user + ":" + password));
@@ -64,50 +63,4 @@ function transcribeFile(file) {
 /*function ibmresults(json) {
 
 }*/
-function extractFile() {
 
-    let input = document.getElementById('transcriptFile');
-
-    let file = input.files[0];
-
-    console.log(file);
-    console.log("This");
-
-    if(!file) {
-        //raise and error somewhere
-    }else {
-        transcribeFile(file);
-    }
-
-}
-
-function transcribeFile(file) {
-    let results = document.getElementById('results');
-    let headers = new Headers();
-    let user= "c17a629b-a1fd-476f-87cf-3869c3ff0abd";
-    let password= "lEQvDlNJTBeI";
-    //let header = {'Content-Type': 'audio/flac','Transfer-Encoding': 'chunked'};
-    let query = '?keywords=several,tornadoes,touch&max_alternatives=3&keywords_threshold=0.5';
-    let url = 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize';
-
-    headers.set('Authorization', 'Basic ' + btoa(user + ":" + password));
-    headers.set('Content-Type', 'audio/mp3');
-   // headers.set('Transfer-Encoding', 'chunked');
-
-    fetch(url, {
-        method: 'POST',
-        headers: headers,
-        body: file
-    }).then((response) => {
-        console.log('here now');
-        console.log(response.body);
-        return response.json();
-    }).then((json) => {
-        console.log('here 2');
-        results.innerText = JSON.stringify(json);
-        console.log(JSON.stringify(json))
-    }).catch((error) => {
-        console.log(error)
-    })
-}
->>>>>>> a31bea1556318dbce2d9a4c2c3fa02ae81b7f69c
